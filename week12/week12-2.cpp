f4f272ad6e69bf764223378baa3710204b3f0e7c
labismallnew <144758479+labismallnew@users.noreply.github.com>
2024-11-28 16:25:52 +0800

    week12

---
+++ week12/week12-2.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        //ListNode* ans = list1;宣告答案,(1)亂接到list1
+        //ListNode* ans = new ListNode(99);(2)新增值是99
+        ListNode* ans = new ListNode(99,new ListNode(88));
+        //(3)新增1個值是99且右邊又新增1個90
+        return ans;
+    }
+};
